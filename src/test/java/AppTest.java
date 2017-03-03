98b512dcc95cfb48cfeb1caabc285239b34ca60b
CrazyBBB <myamamtist@gmail.com>
2017-03-04 02:37:53 +0900

    初期コミット

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by m-yamamt on 2017/03/04.
+ */
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void testSomeLibraryMethod() {
+        assertEquals("Hello Gradle", App.getMessage());
+    }
+}
