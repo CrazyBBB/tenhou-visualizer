d28cd53d958bc4a3f35312b680b44ba2a80e321b
CrazyBBB <myamamtist@gmail.com>
2017-03-04 19:26:12 +0900

    牌譜解析の初期化部分を少し実装

--- src/main/java/Analyzer.java
+++ src/main/java/Analyzer.java
@@ -1,13 +1,75 @@
-import org.w3c.dom.Document;
+import org.w3c.dom.*;
 
+import java.io.IOException;
+import java.net.URLDecoder;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by m-yamamt on 2017/03/04.
  */
 public class Analyzer {
-    public static ArrayList<Scene> findOriScenes(Document document) {
+    public static ArrayList<Scene> findOriScenes(Document document) throws IOException {
         ArrayList<Scene> oriScenes = new ArrayList<>();
+
+        String[] players = new String[4];
+        int[] syanten = {13, 13, 13, 13};
+        int[][] tehai = new int[4][34];
+
+        Element element = document.getDocumentElement();
+        NodeList nodeList = element.getChildNodes();
+        int len = nodeList.getLength();
+        int index = 0;
+
+        while (index < len) {
+            Node node = nodeList.item(index);
+            String nodeName = node.getNodeName();
+
+            if ("SHUFFLE".equals(nodeName)) {
+
+            } else if ("GO".equals(nodeName)) {
+
+            } else if ("UN".equals(nodeName)) {
+                NamedNodeMap attributes = node.getAttributes();
+                for (int i = 0; i < attributes.getLength(); i++) {
+                    Node attribute = attributes.item(i);
+                    String key = attribute.getNodeName();
+                    if (key.matches("n\\d")) {
+                        String name = URLDecoder.decode(attribute.getNodeValue(), "UTF-8");
+                        players[Integer.parseInt(key.substring(1))] = name;
+                    }
+                }
+            } else if ("TAIKYOKU".equals(nodeName)) {
+
+            } else if ("INIT".equals(nodeName)) {
+                for (int i = 0; i < 4; i++) {
+                    Arrays.fill(tehai[i], 0);
+                }
+
+                NamedNodeMap attributes = node.getAttributes();
+                for (int i = 0; i < attributes.getLength(); i++) {
+                    Node attribute = attributes.item(i);
+                    String key = attribute.getNodeName();
+                    if (key.matches("hai\\d")) {
+                        int playerId = Integer.parseInt(key.substring(3));
+                        String value = attribute.getNodeValue();
+                        String[] hais = value.split(",");
+
+                        for (int j = 0; j < 13; j++) {
+                            tehai[playerId][Integer.parseInt(hais[j])]++;
+                        }
+                    }
+                }
+            } else if ("AGARI".equals(nodeName)) {
+
+            } else if ("RYUUKYOKU".equals(nodeName)) {
+
+            } else if ("N".equals(nodeName)) {
+
+            } else {
+
+            }
+        }
 
         return oriScenes;
     }
